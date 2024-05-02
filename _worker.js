17cb0d1a4e5af5ee0af96fe1e2bb51292b6a26ee
liMilCo <91998494+liMilCo@users.noreply.github.com>
2024-05-02 03:55:34 +0330

    Add files via upload

---
+++ _worker.js
@@ -0,0 +1,17 @@
+addEventListener(
+   "fetch", event => {
+       
+       const ip = event.request.headers.get('cf-connecting-ip') || event.request.headers.get('x-forwarded-for') || (event.request.socket && event.request.socket.remoteAddress);
+       let url = new URL(event.request.url);
+       const worker_domain=url.hostname;
+       url.hostname = "host.parshast.xyz";                        
+       url.protocol = event.request.headers.get('x-forwarded-proto') || "https";
+       let request = new Request(url, event.request);
+       if (ip)
+        request.headers.set('cf-connecting-ip', ip);
+        request.headers.set('Host', worker_domain);
+       event.respondWith(
+           fetch(request)
+       )
+   }
+)
