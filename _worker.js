92897609455925a4e6347e17e580fab366219570
liMilCo <91998494+liMilCo@users.noreply.github.com>
2024-05-02 04:32:23 +0330

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -1,18 +1,11 @@
 
 export default {
 
+let url = new URL(event.request.url);
+                        url.hostname = 'host.parshast.xyz';
+                        url.protocol = 'https:';
+                        request = new Request(url, request);
+                        return await fetch(request);
        
-       const ip = event.request.headers.get('cf-connecting-ip') || event.request.headers.get('x-forwarded-for') || (event.request.socket && event.request.socket.remoteAddress);
-       let url = new URL(event.request.url);
-       const worker_domain=url.hostname;
-       url.hostname = "host.parshast.xyz";                        
-       url.protocol = event.request.headers.get('x-forwarded-proto') || "https";
-       let request = new Request(url, event.request);
-       if (ip)
-        request.headers.set('cf-connecting-ip', ip);
-        request.headers.set('Host', worker_domain);
-       event.respondWith(
-           fetch(request)
-       )
 
 }
