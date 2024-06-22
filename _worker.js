365f25ce03da7a545cc1ebab7ffa5c95d5d5a533
liMilCo <91998494+liMilCo@users.noreply.github.com>
2024-06-23 02:00:23 +0330

    Update v2

--- _worker.js
+++ _worker.js
@@ -9,7 +9,7 @@ export default {
     async fetch(request, env, ctx) {
        
                         const url = new URL(request.url);
-                        url.hostname = 'host.parshast.xyz';
+                        url.hostname = env.HOST || 'host.parshast.xyz';
                         url.protocol = 'https:';
                         request = new Request(url, request);
                         return fetch(request);
