65734c9f5a1841d4cd572cc1e9320f01945bc38c
liMilCo <91998494+liMilCo@users.noreply.github.com>
2024-11-28 00:07:10 +0330

    _worker.js

--- _worker.js
+++ _worker.js
@@ -1,11 +1,5 @@
 export default {
 
-     /**
-     * @param {import("@cloudflare/workers-types").Request} request
-     * @param {{UUID: string, PROXYIP: string, DNS_RESOLVER_URL: string}} env
-     * @param {import("@cloudflare/workers-types").ExecutionContext} ctx
-     * @returns {Promise<Response>}
-     */
     async fetch(request, env, ctx) {
        
                         const url = new URL(request.url);
